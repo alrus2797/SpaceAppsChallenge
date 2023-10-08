a2e73c5cf05499f8fe12dada3a811c43415bba81
sharon rosas <sharonrosas@ravn.co>
2023-10-08 15:16:36 -0500

    feat: add spacecraft router

--- SAC.api/main.py
+++ SAC.api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from controllers.item_controller import router as item_router
 from controllers.planet_controller import router as planet_router
+from controllers.spacecraft_controller import router as spacecraft_router
 
 app = FastAPI()
 
 app.include_router(item_router)
 app.include_router(planet_router)
+app.include_router(spacecraft_router)
